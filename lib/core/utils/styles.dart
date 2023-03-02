a4307cedc5a4e0d03f86ed4f1db1fbb5ca102101
sohaib077 <sohaibmahmoud06@gmail.com>
2023-03-02 22:05:46 +0200

    add text style

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,9 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class Styles{
-  static TextStyle titleMedium = TextStyle(
+
+  static TextStyle textStyleSemiBold18 = TextStyle(
     fontSize: 18.sp,
     fontWeight: FontWeight.w600,
     letterSpacing: 1,
   );
+
+  static TextStyle textStyle30 = TextStyle(
+    fontSize: 30.sp,
+    letterSpacing: 1,
+  );
+
 }
--- lib/features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,10 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/features/Home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:bookly/features/Home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../../core/utils/assets_data.dart';
 import '../../../../../core/utils/styles.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -18,10 +20,33 @@ class HomeViewBody extends StatelessWidget {
         const CustomAppBar(),
         const FeaturedBooksListView(),
         Padding(
-          padding: EdgeInsets.only(top: 50.h , right: 10.w , left: 10.w),
+          padding:
+              EdgeInsets.only(top: 40.h, right: 10.w, left: 10.w, bottom: 30.h),
           child: Text(
-              'Best Seller',
-            style: Styles.titleMedium,
+            'Best Seller',
+            style: Styles.textStyleSemiBold18,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.w),
+          child: Row(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.15,
+                child: AspectRatio(
+                  aspectRatio: 2.5 / 4,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: const DecorationImage(
+                        image: AssetImage(AssetsData.testImage),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
